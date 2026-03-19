0b5f77c4cc4528d2d19f2e5d394398d5ed893536
VictorEscDiaz <temmo375@gmail.com>
2026-03-19 16:35:19 -0600

    primera correccion

--- app.py
+++ app.py
@@ -130,4 +130,5 @@ def qr_directo():
     return send_file(buffer, mimetype="image/png")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
